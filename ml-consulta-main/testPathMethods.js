ab6dddb9dd4741189a02d28f8558b57a46a45bad
camimtin <c_mtin@hotmail.com>
2021-09-06 20:45:35 -0300

    Subo lo visto con Diego

---
+++ ml-consulta-main/testPathMethods.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+// console.log(`path.join()`, path.join())
+// console.log(`path.join("/views/home.html")`, path.join("/views/home.html"))
+
+// console.log(`path.resolve()`, path.resolve("", "views/home.html"));
+
+// console.log(`__dirname`, __dirname);
+// console.log(`path.join(__dirname, )`, path.join(__dirname, "/views/home.html"))
+
+console.log(`path.resolve()`, path.resolve("views/home.html"));
