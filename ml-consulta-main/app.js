3241b66dc4e5f3102ba4c508031bf39148963827
camimtin <c_mtin@hotmail.com>
2021-09-13 20:41:21 -0300

    Subo hasta donde llegué

--- ml-consulta-main/app.js
+++ ml-consulta-main/app.js
@@ -8,7 +8,6 @@ app.use(express.static('public'));
 // app.get('/', (req, res) => res.send("Hola mundo!"));
 app.get('/', (req, res) => res.sendFile(path.resolve("views/home.html")));
 
-app.listen(process.env.PORT
-     || 3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('App listening on port http://localhost:3000/');
 });
